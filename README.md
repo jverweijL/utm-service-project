75fa5717c9dd4844baf048389e0845d5a597c46b
Jan Verweij <jan.verweij@liferay.com>
2019-08-09 10:08:58 +0200

    added processed param to maintain utm params

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 This module will turn utm_* params into cookies so you can use them with the segment builder to display custom content on your page.
 
-Current downside is that it redirects to the page and the params are lost.
-This will be fixed in the next update.
+Since we want to maintain the original utm params for analytics we'll add a param to make sure it's not going into an infinite loop.
--- src/main/java/com/liferay/demo/utm/service/UtmService.java
+++ src/main/java/com/liferay/demo/utm/service/UtmService.java
@@ -11,7 +11,6 @@ import org.osgi.service.component.annotations.Component;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Collections;
 
@@ -26,6 +25,8 @@ import java.util.Collections;
 )
 public class UtmService implements LifecycleAction {
 
+	private static String PROCESSED_UTM = "pUTM";
+
 	@Override
 	public void processLifecycleEvent(LifecycleEvent lifecycleEvent)
 			throws ActionException {
@@ -36,21 +37,32 @@ public class UtmService implements LifecycleAction {
 
 		HttpServletResponse response = lifecycleEvent.getResponse();
 		HttpServletRequest request = lifecycleEvent.getRequest();
+
 		try {
 			boolean containsUTM = false;
-			for (String param : Collections.list(request.getParameterNames())) {
-				if (param.toLowerCase().startsWith("utm_")) {
-					//TODO possible bug with same utm_ params
-					Cookie cookie = new Cookie(param,request.getParameter(param));
-					response.addCookie(cookie);
-					containsUTM = true;
-				}
+			boolean processedUTM = false;
+
+			if (request.getParameter(PROCESSED_UTM) != null)
+			{
+				processedUTM = true;
 			}
 
-			if (containsUTM) {
-				StringBuilder requestURL = new StringBuilder(request.getRequestURL().toString());
-				String queryString = request.getQueryString().replace("utm_","_");
-				response.sendRedirect(requestURL.append('?').append(queryString).toString());
+			if (!processedUTM) {
+				for (String param : Collections.list(request.getParameterNames())) {
+					if (param.toLowerCase().startsWith("utm_")) {
+						//TODO possible bug with same utm_ params??
+						Cookie cookie = new Cookie(param, request.getParameter(param));
+						response.addCookie(cookie);
+						containsUTM = true;
+					}
+				}
+
+				if (containsUTM) {
+					StringBuilder requestURL = new StringBuilder(request.getRequestURL().toString());
+					String queryString = request.getQueryString() + "&" + PROCESSED_UTM + "=1";
+					response.sendRedirect(requestURL.append('?').append(queryString).toString());
+					//TODO possible bug need to add path??
+				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
